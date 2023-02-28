954eaf2bbe9ff8ff50664b162253d40b697c7426
Irene Brignoli <ire.brignoli@gmail.com>
2023-02-28 16:41:30 +0100

    circle completi - inizio dei bonus

--- js/main.js
+++ js/main.js
@@ -10,7 +10,10 @@ const imgCarousel = [
 
 const imgListDom = document.querySelector('.img-list');
 
+const circleContainerDom = document.querySelector('.circle-container');
+
 let sliderContent = ''; //creo un contenuto per la mia lista di img
+let circleContent = ''; //creo un contenuto per la il cicrlce container
 
 for(let i = 0; i < imgCarousel.length; i++){
 
@@ -20,16 +23,23 @@ for(let i = 0; i < imgCarousel.length; i++){
 
   sliderContent += newImgBox; //il cotnenuto dento img-list è uguale al nuovo elemento div
 
+  const newcircle = `<div class="circle"></div>`;
+
+  circleContent += newcircle;
+
 }
 
 imgListDom.innerHTML = sliderContent;
+circleContainerDom.innerHTML = circleContent;
 
 const imgsBoxDom = document.getElementsByClassName('img-box');
+const circleDom = document.getElementsByClassName('circle');
 
 //attiviamo le img nel carousel
 
 let imageOn = 0; //creo una variabile per andare a modificare lo stato attivo dell img
 imgsBoxDom [imageOn].classList.add('show'); //se è in ON gli do la classe show
+circleDom [imageOn].classList.add('circle-current');
 
 const prevDom = document.querySelector('#prev-up'); //collego i pulsanti al dom
 const nextDom = document.querySelector('#next-down');
@@ -43,10 +53,12 @@ nextDom.addEventListener('click',
     if (imageOn < imgsBoxDom.length - 1 ){ //se il valore di imagOn è minore della lunghezza totale raggiunta
 
       imgsBoxDom [imageOn].classList.remove('show'); //nascondi img
+      circleDom [imageOn].classList.remove('circle-current');
       
       imageOn++;
 
       imgsBoxDom [imageOn].classList.add('show'); //mostra prossima img
+      circleDom [imageOn].classList.add('circle-current');
 
       prevDom.classList.remove('hide'); //mostra pulsante per tornare indietro
 
@@ -67,10 +79,12 @@ prevDom.addEventListener('click',
     if(imageOn > 0){
 
       imgsBoxDom [imageOn].classList.remove('show');
+      circleDom [imageOn].classList.remove('circle-current');
 
       imageOn--;
 
       imgsBoxDom [imageOn].classList.add('show');
+      circleDom [imageOn].classList.add('circle-current');
 
       nextDom.classList.remove('hide');
 
